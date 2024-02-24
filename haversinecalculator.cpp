308bad033f188f052767a63ac82a2af93a63926b
jacobhayes64 <jacobt.hayes64@gmail.com>
2024-02-23 23:36:44 -0500

    Add project files.

---
+++ haversinecalculator.cpp
@@ -0,0 +1,34 @@
+// haversinecalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <math.h>
+#include <string>
+
+
+void printmenu() {
+	
+}
+
+double getinput() {
+	std::cout << "Enter the number;"
+	std::getline(std::cin, )
+}
+double centralangle(double distance, double radius) {
+	double onot = distance / radius;
+	return onot;
+}
+
+double haversine(double onot, ){ }
+
+double distance(double haversine) {
+
+}
+
+int main()
+{
+	std::cout << "|Haversine Calculator|" << std::endl;
+ // central angle Onot
+
+
+}
