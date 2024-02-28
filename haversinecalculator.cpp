b218ea1ec6cd8d176efd4623792b60b7ede87423
jacobhayes64 <jacobt.hayes64@gmail.com>
2024-02-28 16:43:24 -0500

    incomplete

--- haversinecalculator.cpp
+++ haversinecalculator.cpp
@@ -4,9 +4,54 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include <future>
+
+int temp2;
+
+int grabinput() {
+    std::cin.clear();
+    std::cin >> temp2;
+    std::cin.clear();
+    return temp2;
+}
 
 
 void printmenu() {
+    std::cout << "....." << std::endl << "(1) View input(s)\n(2) Add new input\n(3) Delete input\n(4) Clear inputs\n(5) Exit program" << endl << "....." << endl;
+    int menuinput = grabinput();
+    std::cout << endl;
+    switch (menuinput) {
+    case 1:
+        printarray();
+        printmenu();
+        break;
+    case 2:
+    {
+        std::future<bool> fut = std::async(grabstring);
+        bool ret = fut.get();
+        if (ret) {
+            printmenu();
+        }
+        else {
+            std::cout << "ERROR NO STRING";
+        }
+        break;
+    }
+    case 3:
+        deleteinput();
+        printmenu();
+        break;
+    case 4:
+        cleararray();
+        printmenu();
+        break;
+    case 5:
+        killprogram();
+        break;
+    default:
+        std::cout << "Enter a valid option" << endl;
+        printmenu();
+        break;
 	
 }
 
@@ -14,21 +59,17 @@ double getinput() {
 	std::cout << "Enter the number;"
 	std::getline(std::cin, )
 }
-double centralangle(double distance, double radius) {
-	double onot = distance / radius;
-	return onot;
-}
 
-double haversine(double onot, ){ }
+long double haversine(double long1, double long2, double lat1, double lat2, long double radius){ 
+	long double distance;
+	distance = 2 * radius * asin(sqrt(pow(sin, 2))
 
-double distance(double haversine) {
-
+	return distance;
 }
 
 int main()
 {
 	std::cout << "|Haversine Calculator|" << std::endl;
- // central angle Onot
 
 
 }
